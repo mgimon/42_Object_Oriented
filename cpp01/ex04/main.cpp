76caa6f6859485df37ae74a53f48579fcbb29498
mgimon-asus <m.gimoncaballero@gmail.com>
2025-03-25 04:25:15 +0100

    cpp01 ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,27 +1,58 @@
-#include "CopyMaker.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
 
-int main()
+std::string	change_line(std::string line, std::string s1, std::string s2)
 {
-	std::string 	filename = "test";
+    std::string result;
+    size_t pos = 0;
+    size_t last_pos = 0;
+
+    // While s1 appears in line
+    while ((last_pos = line.find(s1, pos)) != std::string::npos)
+    {
+        // Add before s1
+        result.append(line.substr(pos, last_pos - pos));
+
+        // Add s2 instead of s1
+        result.append(s2);
+
+        // Move pos beyond s1
+        pos = last_pos + s1.length();
+    }
+
+    // Add after last s1 (or from 0 if no s1)
+    result.append(line.substr(pos));
+    return result;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+		return (std::cerr << "Please, execute with three arguments" << std::endl, 1);
+
+	std::string 	filename = argv[1];
 	std::string		new_filename = filename + ".replace";
 	std::string		line;
-
     std::ifstream	file(filename.c_str());
 
 	if (!file) 
 		return (std::cerr << "Error opening file" << std::endl, 1);
 
 	std::ofstream	newfile(new_filename.c_str());
+	std::string		s1 = argv[2];
+	std::string		s2 = argv[3];
 
 	if (!newfile)
 		return (std::cerr << "Error creating newfile" << std::endl, 1);
 
-	// Copiar línea por línea
 	while (std::getline(file, line))
+	{
+		line = change_line(line, s1, s2);
 		newfile << line << std::endl;
+	}
 
-	file.close();  // Cerrar el archivo
+	file.close();
 	newfile.close();
-
-	return 0;
+	return (0);
 }
